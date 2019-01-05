73fb266d8abeea0233e37376550eec4ff9ebe5ae
Rakesh Baruah <rbaruah@mac.com>
2019-01-05 15:47:11 -0600

    Connect 'load sample fish' onclick function to menu page through Object.Keys.Map()

---
+++ src/components/Fish.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Fish extends React.Component {
+    render() {
+        return (
+            <div className="fish">
+                <p>fish Emoji</p>
+            </div>
+        )
+    }
+}
+
+export default Fish;

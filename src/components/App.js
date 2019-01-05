d6fcdb279182281f34825fc4bec40cad0edf4552
Rakesh Baruah <rbaruah@mac.com>
2019-01-05 15:18:09 -0600

    Updating order state: loading sample fish dataset for inventory page.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,26 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        orders: {}
+    };
+    addFish = fish => {
+        // 1. Take a copy of existing state
+        const fishes = {...this.state.fishes};
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set new fishes object to state
+        this.setState({
+            fishes: fishes
+        });
+    }
+    loadSampleFishes = () => {
+        this.setState({fishes: sampleFishes});
+    }
     render() {
         return (
             <div className="catch-of-the-day">
@@ -12,7 +30,10 @@ class App extends React.Component {
                             tagline2 = "Kesh is cool"/>
                 </div>                
                 <Order />
-                <Inventory />
+                <Inventory 
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                />
             </div>
         )
     }
